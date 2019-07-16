bd3cff3b632ebdcc81ccab4b266547c2417d8782
Michael Twitt <michael.twitt@hotmail.com>
2019-07-16 12:57:17 +1000

    navbar and purchase button components created

--- src/App.js
+++ src/App.js
@@ -16,35 +16,21 @@ function App() {
   return (
     <>
     <div className='navbar'>
-      <h1 className="navbar-brand navbar-item">Jo Brimblecombe</h1>
-      <p className="navbar-item navbar-end">
-        Modern CSS framework based on{' '}
-        <a className='navbar-end navbar-divider' href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">
-          Flexbox
-                  </a>
-      </p>
-
-      <div className="field">
-        <div className="control">
-          <input className="input" type="text" placeholder="Input" />
+        <h1 className="navbar-brand navbar-item">Jo Brimblecombe</h1>
+        <div className="navbar-end ">
+          <div className='navbar-item'>
+          <a className='navbar-item navbar-link'><Route exact path="/blog" component={Blog}>Blog</Route></a>
+          <a className='navbar-item navbar-link'><Route exact path="/books/" component={Books}>Books</Route></a>
+          <a className='navbar-item navbar-link'><Route exact path="/newsletter" component={Newsletter}>Newsletter</Route></a>
+          </div>
         </div>
       </div>
-      </div>
-
-      <div className="field">
-        <p className="control">
-          <span className="select">
-            <select>
-              <option>Select dropdown</option>
-            </select>
-          </span>
-        </p>
-      </div>
-
       <div className="buttons">
-        <a className="button is-primary">Primary</a>
-        <a className="button is-link">Link</a>
+        <a className="button is-link">Purchase</a>
       </div>
+      {/* <Route exact path ="/login" component={LoginForm} /> */}
+      <Route exact path="/login" component={ValidatedLoginForm}/>
+      <Route exact path="/books/:slug" component={SingleBook}/>
     </>
   );
 }
