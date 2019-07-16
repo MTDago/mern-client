e6225ca26b7af0d8ac7c2b896a1be93869c544eb
Michael Twitt <michael.twitt@hotmail.com>
2019-07-16 15:23:01 +1000

    Accept this

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,85 +1,37 @@
-import React from 'react';
-import '../App.sass'
-import { Route, Switch } from 'react-router-dom'
-import Books from '../pages/Books'
-import Blog from '../pages/Blog'
-import Newsletter from './Newsletter'
-function Navbar() {
-    return (
-    <>
-        <section class="hero is-info is-large">
-        <div class="hero-head">
-            <nav class="navbar">
-            <div class="container">
-                <div class="navbar-brand">
-                <a class="navbar-item is-active">
-                    Jo Brimblecombe
-                </a>
-                <span class="navbar-burger burger" data-target="navbarMenuHeroB">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </span>
-                </div>
-                <div id="navbarMenuHeroB" class="navbar-menu">
-                <div class="navbar-end">
-                    <a class="navbar-item">
-                    Books
-                    </a>
-                    <a class="navbar-item">
-                    Blogs
-                    </a>
-                    <a class="navbar-item">
-                    Newsletter
-                    </a>
-                    <span class="navbar-item">
-                    </span>
-                </div>
-                </div>
-            </div>
-            </nav>
-        </div>
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import '../App.sass';
 
-        <div class="hero-body">
-            <div class="container has-text-centered">
-            <p class="title">
-                About Me
-            </p>
-            <p className='container has-text-left'>
-            "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamcatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray."
-            </p>
-            </div>
-        </div>
+export default class Navbar extends Component {
+    state = {
+        isOpen: false
+    }
 
-        <div class="hero-foot">
-            <nav class="tabs is-boxed is-fullwidth">
-            <div class="container">
-                <ul>
-                <li class="is-active">
-                    <a>Overview</a>
-                </li>
-                <li>
-                    <a>Modifiers</a>
-                </li>
-                <li>
-                    <a>Grid</a>
-                </li>
-                <li>
-                    <a>Elements</a>
-                </li>
-                <li>
-                    <a>Components</a>
-                </li>
-                <li>
-                    <a>Layout</a>
-                </li>
-                </ul>
-            </div>
-            </nav>
+    handleToggle = () => {
+        this.setState({isOpen:!this.state.isOpen})
+    }
+    
+    render() {
+        return <div className="navbar-menu">
+                <div>
+                    <Link to="/">
+                        <h2 className='navbar-item navbar-brand'>Jo Brimblecombe</h2>   
+                    </Link>
+                </div>
+                <>
+                    <a>    
+                        <Link className='navbar-end navbar-item' to="/aboutMe">About Me</Link>
+                    </a>
+                    <a>
+                        <Link className='navbar-end navbar-item' to="/books">Books</Link>
+                    </a>
+                    <a>
+                        <Link className='navbar-end navbar-item' to="/blog">Blog</Link>
+                    </a>
+                    <a>
+                        <Link className='navbar-end navbar-item' to="/newsletter">Newsletter</Link>
+                    </a>
+                </>
         </div>
-        </section>
-    </>
-  );
+    }
 }
-
-export default Navbar;
