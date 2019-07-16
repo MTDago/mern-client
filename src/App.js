407246e36eaab20175760b55f5ae834e28ea4099
Michael Twitt <michael.twitt@hotmail.com>
2019-07-16 14:58:21 +1000

    navbar updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import './App.sass'
-import logo from './logo.svg';
-// import './App.css';
 import Navbar from './components/Navbar';
 import { Route, Switch } from 'react-router-dom'
 import AboutMe from './pages/AboutMe';
@@ -11,28 +8,21 @@ import Newsletter from './components/Newsletter'
 // import LoginForm from './components/LoginForm';
 import ValidatedLoginForm from './components/ValidateLoginForm';
 import SingleBook from './components/SingleBook';
+import './App.css';
 
 function App() {
-  return (
-    <>
-    <div className='navbar'>
-        <h1 className="navbar-brand navbar-item">Jo Brimblecombe</h1>
-        <div className="navbar-end ">
-          <div className='navbar-item'>
-          <a className='navbar-item navbar-link'><Route exact path="/blog" component={Blog}>Blog</Route></a>
-          <a className='navbar-item navbar-link'><Route exact path="/books/" component={Books}>Books</Route></a>
-          <a className='navbar-item navbar-link'><Route exact path="/newsletter" component={Newsletter}>Newsletter</Route></a>
-          </div>
-        </div>
-      </div>
-      <div className="buttons">
-        <a className="button is-link">Purchase</a>
-      </div>
-      {/* <Route exact path ="/login" component={LoginForm} /> */}
-      <Route exact path="/login" component={ValidatedLoginForm}/>
-      <Route exact path="/books/:slug" component={SingleBook}/>
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            <Route exact path="/aboutMe" component={AboutMe}/>
+            <Route exact path="/books/" component={Books}/>
+            <Route exact path="/blog" component={Blog}/>
+            <Route exact path="/newsletter" component={Newsletter}/>
+            {/* <Route exact path ="/login" component={LoginForm} /> */}
+            <Route exact path="/login" component={ValidatedLoginForm}/>
+            <Route exact path="/books/:slug" component={SingleBook}/>
+        </>
+    );
 }
 
 export default App;
--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,26 +1,10 @@
 import React from 'react'
 import Title from '../components/Title'
+import '../App.sass'
 
-export default function BookList({ books }) {
-    const Book = () => {}
-    // if (books.length === 0) {
-    //     return (
-    //         <div className='empty-search'>
-    //             <h3>Unfortunately nothing matched your search</h3>
-    //         </div>
-    //     )
-    // }
+export default function Books() {
     return (
-        <section className="blogs">
-            <Title title="My Books" />
-            {/* <section className='bookslist'>
-                <div className='bookslist-center'>
-                    {books.map(item => {
-                        return <Book key={item.id} book={item} />
-                    })}
-                </div>
-            </section> */}
-        </section>
+        <></>
     )
 }
 
