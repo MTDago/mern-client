981106b916e3e9fe058e0db5e5c79fb26fcbf89c
Michael Twitt <michael.twitt@hotmail.com>
2019-07-17 10:03:26 +1000

    Updated layout

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Title from './Title'
+import Title from '../components/Title'
 import axios from 'axios'
 
 const blogAPI = "https://mern-server-deployment.herokuapp.com/blogs"
