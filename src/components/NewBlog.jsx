1a0cc124fd036700f69da1950dc801b6d57c990a
NikChao <leodabrim@gmail.com>
2019-07-18 10:31:31 +1000

    Tags now work a little bit better and get posted to DB

--- src/components/NewBlog.jsx
+++ src/components/NewBlog.jsx
@@ -3,58 +3,57 @@ import Title from '../components/Title';
 import '../App.sass';
 import { blogAPI } from '../API/init';
 import axios from 'axios';
-import {Redirect} from 'react-router-dom'
+import { Redirect } from 'react-router-dom';
 
 export default class NewBook extends Component {
-   constructor(props) {
+    constructor(props) {
         super(props);
         this.state = {
             title: '',
             content: '',
-            tags: '',
             tagArray: []
         };
     }
 
-
-
     handleSubmit = event => {
-        event.preventDefault()
+        event.preventDefault();
         const { title, content } = this.state;
-        const tags = this.tagArray
+        const tags = this.state.tagArray;
         axios.post(blogAPI, {
             title: title,
             content: content,
             tags: tags,
             date: Date.now
-        }).then(
-            <Redirect to="/aboutMe" />
-        )
-        console.log('Something happened')
+        });
+        // THIS IS WHERE YOU NEED TO REDIRECT
+        console.log('Something happened');
     };
 
     handleChange = event => {
         this.setState({
             [event.target.name]: event.target.value
-        })
-    }
+        });
+    };
 
     handleTags = event => {
-        event.preventDefault()
-        let newTag = document.getElementById("tags").value
-
-
+        event.preventDefault();
+        let newTag = document.getElementById('tags').value;
         this.setState({
             tagArray: [...this.state.tagArray, newTag]
-        })
+        });
+    };
 
-
-        console.log(newTag)
-        console.log(this.state.tagArray)
-    }
+    deleteTag = event => {
+        this.setState(state => {
+            const tagArray = state.tagArray.filter((item, j) => event !== j);
+            return {
+                tagArray
+            };
+        });
+    };
 
     render() {
-        const {title, content, tags} = this.state
+        const { title, content, tags } = this.state;
         return (
             <div>
                 <Title title="New Blog Post:" />
@@ -79,7 +78,9 @@ export default class NewBook extends Component {
                             onChange={this.handleChange}
                         />
 
-                        <br/><br/><br/>
+                        <br />
+                        <br />
+                        <br />
 
                         {/* Content*/}
                         <label
@@ -95,33 +96,56 @@ export default class NewBook extends Component {
                             type="text"
                             value={content}
                             onChange={this.handleChange}
-                        ></textarea>
+                        />
 
-                        <br/><br/><br/>
+                        <br />
+                        <br />
+                        <br />
 
                         {/* Tags */}
-                            {this.state.tagArray.map(tag => 
-                            <p key={this.state.tagArray.indexOf(tag)}>{tag}</p>)}
-                            
-                            <label
-                                className="label has-text-centered is-uppercase"
-                                htmlFor="tags"
-                            >
-                                Tags
-                            </label>
-                            <input
-                                className="input is-rounded"
-                                type="text"
-                                name="tags"
-                                placeholder="Enter a tag"
-                                id="tags"
-                            />
-                            <button className="button is-rounded" type="button" id="tagbutton" onClick={this.handleTags}>
-                            Add Tag
-                            </button>
 
-                        <br/><br/><br/>
-                    
+                        <label
+                            className="label has-text-centered is-uppercase"
+                            htmlFor="tags"
+                        >
+                            Tags
+                        </label>
+                        <input
+                            className="input is-rounded"
+                            type="text"
+                            name="tags"
+                            placeholder="Enter a tag"
+                            id="tags"
+                            onKeyPress={e => {
+                                e.target.keyCode === 13 && e.preventDefault();
+                            }}
+                        />
+                        <button
+                            className="button is-rounded"
+                            type="button"
+                            id="tagbutton"
+                            onClick={this.handleTags}
+                        >
+                            Add Tag
+                        </button>
+
+                        {this.state.tagArray.map((tag, index) => (
+                            <a
+                                key={this.state.tagArray.indexOf(tag)}
+                                class="button is-danger is-outlined"
+                                onClick={() => this.deleteTag(index)}
+                            >
+                                <span>{tag}</span>
+                                <span class="icon is-small">
+                                    <i class="fas fa-times" />
+                                </span>
+                            </a>
+                        ))}
+
+                        <br />
+                        <br />
+                        <br />
+
                         <button className="button is-rounded" type="submit">
                             Post
                         </button>
