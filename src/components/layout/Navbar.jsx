2068e08adb910462b306939f9015a28b9f84c753
NikChao <leodabrim@gmail.com>
2019-07-24 20:08:03 +1000

    Changed color of Title in navbar for more contrast and changed the tags for new blog

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -11,28 +11,38 @@ export default class Navbar extends Component {
         this.setState({ isOpen: !this.state.isOpen });
     };
 
-     //Render transforms the components into DOM node that the 
+    //Render transforms the components into DOM node that the
     // browser can understand and display to the screen. Navbar is displayed on every page and links to all other pages.
     render() {
         return (
             <nav className="navbar is-dark is-fluid navbar-menu is-active">
                 <div className="navbar-brand navbar-item">
                     <Link to="/about">
-                        <h2 className=" title is-3 navbar-item">Jo Brimblecombe</h2>
+                        <h2 className=" title is-3 navbar-item has-text-light">
+                            Jo Brimblecombe
+                        </h2>
                     </Link>
                 </div>
                 <section className=" navbar-end ">
                     <p className="navbar-end navbar-item">
-                        <Link to="/about"className="navbar-item">About Me</Link>
+                        <Link to="/about" className="navbar-item">
+                            About Me
+                        </Link>
                     </p>
                     <p className="navbar-end navbar-item">
-                        <Link to="/books"className="navbar-item">Books</Link>
+                        <Link to="/books" className="navbar-item">
+                            Books
+                        </Link>
                     </p>
                     <p className="navbar-end navbar-item">
-                        <Link to="/blog" className="navbar-item">Blog</Link>
+                        <Link to="/blog" className="navbar-item">
+                            Blog
+                        </Link>
                     </p>
                     <p className="navbar-end navbar-item">
-                        <Link to="/newsletter"className="navbar-item">Newsletter</Link>
+                        <Link to="/newsletter" className="navbar-item">
+                            Newsletter
+                        </Link>
                     </p>
                 </section>
             </nav>
